e6e83680d32bf1bf7cfc9dcc6e1198f5e3f5dccb
volkan21 <volkanerdogus@gmail.com>
2023-04-11 11:21:55 +0300

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,11 @@
+#alias
+import random
+from matematik import topla,bol as toplamaislemi
+from workshop5 import Human
+#built-in modules
+#import matematik as m
+#package
+print(toplamaislemi(10,20))
+print(random.randint(0,100))
+human1=Human("Mirza")
+human1.talk("Merhaba")
